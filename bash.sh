00ee4c59d2962f0855b9ccac168bd7eb8a0dc0d0
e.skrilnikov <e.skrilnikov@artjoker.team>
2021-12-26 19:04:45 +0200

    fx

--- bash.sh
+++ bash.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 /usr/bin/php /home/u771985207/domains/reviews4china.com/reviews4china/artisan schedule:run 1>> /dev/null 2>&1
+/usr/bin/php /home/u771985207/domains/reviews4china.com/reviews4china/artisan queue:work --queue=high,default
