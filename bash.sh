c1c1fc501d73d29adad25049bdf3096daa36315a
e.skrilnikov <e.skrilnikov@artjoker.team>
2021-12-26 16:42:25 +0200

    test scheduler

--- bash.sh
+++ bash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/php /home/u771985207/domains/reviews4china.com/reviews4china/artisan schedule:run >> /dev/null 2>&1
+/usr/bin/php /home/u771985207/domains/reviews4china.com/reviews4china/artisan schedule:run 1>> /dev/null 2>&1
