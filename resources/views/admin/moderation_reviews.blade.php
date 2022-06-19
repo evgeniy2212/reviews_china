6e45e5b01dfa26d0adf6a4b401b9d5476248f4f5
e.skrilnikov <e.skrilnikov@artjoker.team>
2022-06-19 21:18:11 +0300

    fx

--- resources/views/admin/moderation_reviews.blade.php
+++ resources/views/admin/moderation_reviews.blade.php
@@ -8,8 +8,7 @@
                     <a class="admin-complain {{ (array_key_exists('is_new', $paginateParams) && $paginateParams['is_new'] === '1')
                                             ? 'active'
                                             : ''}}"
-                       name="is_new"
-                       value="1">@lang('service/index.new') ({!! $newReviewsCnt !!})</a>
+                       href="{{ route('admin.moderations.index', ['is_new' => 1]) }}">@lang('service/index.new') ({!! $newReviewsCnt !!})</a>
                 </div>
             </div>
             <div class="adminFilterItem">
@@ -17,8 +16,7 @@
                     <a class="admin-complain {{ (array_key_exists('is_new', $paginateParams) && $paginateParams['is_new'] === '0')
                                             ? 'active'
                                             : ''}}"
-                       name="is_new"
-                       value="0">@lang('service/index.processed') ({!! $processedReviewsCnt !!})</a>
+                       href="{{ route('admin.moderations.index', ['is_new' => 0]) }}">@lang('service/index.processed') ({!! $processedReviewsCnt !!})</a>
                 </div>
             </div>
             <div class="adminFilterItem">
@@ -26,8 +24,7 @@
                     <a class="admin-complain {{ ((array_key_exists('is_new', $paginateParams) && $paginateParams['is_new'] === 'NULL') || !array_key_exists('is_new', $paginateParams))
                                             ? 'active'
                                             : ''}}"
-                       name="is_new"
-                       value="NULL">@lang('service/index.all') ({!! $allReviewsCnt !!})</a>
+                       href="{{ route('admin.moderations.index', ['is_new' => null]) }}">@lang('service/index.all') ({!! $allReviewsCnt !!})</a>
                 </div>
             </div>
         </div>
