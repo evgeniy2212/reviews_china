78b78b43a92f9be00a46134ddcd3274338bfd3f0
e.skrilnikov <e.skrilnikov@artjoker.team>
2022-07-08 21:53:00 +0300

    fx

--- bash.sh
+++ bash.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-echo "Today is 08.07"
 /usr/bin/php /home/u771985207/domains/reviews4china.com/reviews4china/artisan schedule:run 1>> /dev/null 2>&1
 /usr/bin/php /home/u771985207/domains/reviews4china.com/reviews4china/artisan queue:work --queue=high,default
